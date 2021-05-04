11d21b884a2769de2fd7f32b3a046b39e73d4ef1
Miko <kozak.mikolaj@gmail.com>
2021-05-04 10:24:16 +0200

    I'm here now to see into past

--- src/components/Work.js
+++ src/components/Work.js
@@ -20,25 +20,26 @@ const Card = styled.li`
 
 // function setPositions() {}
 
-const Work = ({ index }) => {
-  function clickAction(e) {
-    let all = [...document.querySelectorAll(".work")];
-    let opened = [...document.querySelectorAll(".open")];
-    let index = all.indexOf(e.target);
-    if (!e.target.classList.contains("open")) {
-      all.forEach((el) => (all.indexOf(el) <= index ? el.classList.add("open") : el.classList.remove("open")));
-    } else {
-      all.forEach((el) => {
-        if (opened.length > 1) {
-          all.indexOf(el) > all.indexOf(e.target) && el.classList.remove("open");
-        } else {
-          e.target.classList.remove("open");
-        }
-      });
-    }
-  }
+const Work = ({ index, handler }) => {
+  // function clickAction(e) {
+  //   console.log(e.target);
+  //   let all = [...document.querySelectorAll(".work")];
+  //   let opened = [...document.querySelectorAll(".open")];
+  //   let index = all.indexOf(e.target);
+  //   if (!e.target.classList.contains("open")) {
+  //     all.forEach((el) => (all.indexOf(el) <= index ? el.classList.add("open") : el.classList.remove("open")));
+  //   } else {
+  //     all.forEach((el) => {
+  //       if (opened.length > 1) {
+  //         all.indexOf(el) > all.indexOf(e.target) && el.classList.remove("open");
+  //       } else {
+  //         e.target.classList.remove("open");
+  //       }
+  //     });
+  //   }
+  // }
 
-  return <Card index={index} className="work" onClick={clickAction}></Card>;
+  return <Card index={index} onClick={handler} className="work"></Card>;
 };
 
 export default Work;
--- src/components/Works.js
+++ src/components/Works.js
@@ -13,54 +13,47 @@ const Cards = styled.ul`
 `;
 
 const Works = () => {
-  let [worksList] = useState([
+  let [worksList, setWorkList] = useState([
     {
       name: "vibal",
       focus: false,
-      opened: false,
     },
     {
       name: "superGood",
       focus: false,
-      opened: false,
     },
     {
       name: "lets watch",
       focus: false,
-      opened: false,
     },
     {
       name: "backend",
       focus: false,
-      opened: false,
     },
     {
       name: "mergn",
       focus: false,
-      opened: false,
     },
   ]);
-  // function focusing(e) {
-  //   setWorkList((worksList) => {
-  //     let index = [...document.querySelectorAll(".work")].indexOf(e.target);
-  //     return worksList.map((workObj) => {
-  //       if (workObj === worksList[index]) {
-  //         return (workObj = { ...workObj, focus: true, opened: true });
-  //       } else {
-  //         if (worksList.indexOf(workObj) <= index) {
-  //           return (workObj = { ...workObj, focus: false, opened: true });
-  //         } else {
-  //           return (workObj = { ...workObj, focus: false, opened: false });
-  //         }
-  //       }
-  //     });
-  //   });
-  // }
-  // onClick={focusing}
+
+  function focusing(e) {
+    setWorkList((worksList) => {
+      let index = [...document.querySelectorAll(".work")].indexOf(e.target);
+      worksList.forEach((workObj) => {
+        if (workObj === worksList[index]) {
+          return (workObj = { ...workObj, focus: true });
+        } else {
+          return (workObj = { ...workObj, focus: false });
+        }
+      });
+    });
+  }
+  console.log(worksList);
+
   return (
     <Cards>
       {worksList.map((workObj) => {
-        return <Work key={workObj.name} index={worksList.indexOf(workObj)} name={workObj.name}></Work>;
+        return <Work key={workObj.name} handler={focusing} index={worksList.indexOf(workObj)} name={workObj.name}></Work>;
       })}
     </Cards>
   );
