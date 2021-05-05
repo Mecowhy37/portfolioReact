9292bd2d505f53900ab940d3e9a53510afc02469
Miko <kozak.mikolaj@gmail.com>
2021-05-05 21:12:05 +0200

    branch commit

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,16 +8,14 @@ const Card = styled.li`
   height: 90%;
   border-right: ${({ focus }) => (focus ? "1px solid red" : "1px solid var(--color-prm)")};
   transition: 1s cubic-bezier(0.25, 0.68, 0.21, 0.99);
-  /* left: ${({ index }) => (2 - index) * 40}px; */
   left: ${({ index }) => (4 - index) * 40}px;
-  transform: ${({ open }) => (open ? "translateX(1200px)" : "")};
   &:hover {
     margin-left: 3px;
   }
 `;
 
-function Work({ index, focus, open, onClick }) {
-  return <Card index={index} onClick={onClick} className={`work`} focus={focus} open={open}></Card>;
+function Work({ index, focus, onClick }) {
+  return <Card index={index} className={`work`} focus={focus} onClick={onClick}></Card>;
 }
 
 export default Work;
--- src/components/Works.js
+++ src/components/Works.js
@@ -11,64 +11,69 @@ const Cards = styled.ul`
   list-style: none;
   overflow: hidden;
 `;
-const initialList = [
+const worksList = [
   {
     name: "vibal",
-    focus: false,
-    opened: false,
   },
   {
     name: "superGood",
-    focus: false,
-    opened: false,
   },
   {
     name: "lets watch",
-    focus: false,
-    opened: false,
   },
   {
     name: "backend",
-    focus: false,
-    opened: false,
   },
   {
     name: "mergn",
-    focus: false,
-    opened: false,
   },
 ];
 
 const Works = () => {
-  let [worksList, setWorkList] = useState(initialList);
-  function focusing(e) {
-    setWorkList((worksList) => {
-      let index = [...document.querySelectorAll(".work")].indexOf(e.target);
-      return worksList.map((workObj) => {
-        if (workObj === worksList[index]) {
-          if (workObj.focus && index === 0) {
-            return (workObj = { ...workObj, focus: false, opened: false });
-          } else {
-            return (workObj = { ...workObj, focus: true, opened: true });
-          }
-        } else {
-          if (worksList.indexOf(workObj) <= index) {
-            return (workObj = { ...workObj, focus: false, opened: true });
-          } else {
-            return (workObj = { ...workObj, focus: false, opened: false });
-          }
-        }
-      });
-    });
-  }
-  console.table(worksList);
+  const [activeWork, setActiveWork] = useState(null);
   return (
     <Cards>
-      {worksList.map((workObj) => {
-        return <Work key={workObj.name} index={worksList.indexOf(workObj)} onClick={focusing} name={workObj.name} focus={workObj.focus} open={workObj.opened}></Work>;
+      {worksList.map((workObj, index) => {
+        return (
+          <Work
+            key={workObj.name}
+            index={index}
+            onClick={() => {
+              if (worksList.indexOf(workObj) === 0) {
+                activeWork === workObj.name ? setActiveWork(null) : setActiveWork(workObj.name);
+              } else {
+                setActiveWork(workObj.name);
+              }
+            }}
+            name={workObj.name}
+            focus={activeWork === workObj.name}
+          ></Work>
+        );
       })}
     </Cards>
   );
 };
 
 export default Works;
+
+// let [worksList, setWorkList] = useState(initialList);
+// function focusing(e) {
+//   setWorkList((worksList) => {
+//     let index = [...document.querySelectorAll(".work")].indexOf(e.target);
+//     return worksList.map((workObj) => {
+//       if (workObj === worksList[index]) {
+//         if (workObj.focus && index === 0) {
+//           return (workObj = { ...workObj, focus: false, opened: false });
+//         } else {
+//           return (workObj = { ...workObj, focus: true, opened: true });
+//         }
+//       } else {
+//         if (worksList.indexOf(workObj) <= index) {
+//           return (workObj = { ...workObj, focus: false, opened: true });
+//         } else {
+//           return (workObj = { ...workObj, focus: false, opened: false });
+//         }
+//       }
+//     });
+//   });
+// }
